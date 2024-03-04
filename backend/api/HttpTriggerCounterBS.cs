30c1368f5241d9e968486526685b5ea7e452a729
Blazej <bszosta@outlook.com>
2024-03-04 22:19:54 +0100

    Azure Function Api call

--- backend/api/HttpTriggerCounterBS.cs
+++ backend/api/HttpTriggerCounterBS.cs
@@ -1,57 +1,54 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Cosmos;
-using Newtonsoft.Json;
-using System.IO;
-using System.Net.Http;
-using System.Text;
+using System.Threading.Tasks;
+
 
 namespace Company.Function
 {
-    // public class HttpTriggerCounterBS
-    // {
-    //     private readonly ILogger<HttpTriggerCounterBS> _logger;
-
-    //     public HttpTriggerCounterBS(ILogger<HttpTriggerCounterBS> logger)
-    //     {
-    //         _logger = logger;
-    //     }
-
-    //     [Function("HttpTriggerCounterBS")]
-    //     public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
-    //     {
-    //         _logger.LogInformation("C# HTTP trigger function processed a request.");
-    //         return new OkObjectResult("Welcome to Azure Functions!");
-    //     }
-    // }
-
-
-    public static class HttpTriggerCounterBS
+    public class HttpTriggerCounterBS
     {
-        [FunctionName("HttpTriggerCounterBS")]
-        public static  HttpResponseMessage Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName:"azureresumebsz", containerName:"Counter", Connection ="AzureResumeConnectionString", Id ="1", PartitionKey ="1")] Counter counter,
-            [CosmosDB(databaseName:"azureresumebsz", containerName:"Counter", Connection ="AzureResumeConnectionString", Id ="1", PartitionKey ="1")] out Counter updatedCounter,
-            ILogger log)
-        {
-            log.LogInformation("HTTP trigger function processed a request");
-            
-            updatedCounter = counter;
-            updatedCounter.Count += 1;
+        private readonly ILogger<HttpTriggerCounterBS> _logger;
+        private CosmosClient _cosmosClient;
+        private Container _container;
 
-            var jsonToReturn = JsonConvert.SerializeObject(counter);
-            
-            return new HttpResponseMessage(System.Net.HttpStatusCode.OK){
-                Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
-            };
+        public HttpTriggerCounterBS(ILogger<HttpTriggerCounterBS> logger)
+        {
+            _logger = logger;
+
+            string connectionString = Environment.GetEnvironmentVariable("AzureResumeConnectionString");
+            if(string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("CosmosDB connection string is not set in the environment variables.");
+            }
+
+            _container = _cosmosClient.GetContainer("azureresumebsz","Counter");
         }
 
-    }      
+        [Function("HttpTriggerCounterBS")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
 
+            try
+            {
+                var response = await _container.ReadItemAsync<dynamic>("1", new PartitionKey("1"));
+                var item = response.Resource;
 
+                return new OkObjectResult(item);
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                _logger.LogWarning($"Item not found. Details: {ex.Message}");
+                return new NotFoundResult();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred: {ex.Message}");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
 }
