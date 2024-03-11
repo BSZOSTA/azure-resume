e80b137d4200607537a9c14567f408e481bbf702
Blazej <bszosta@outlook.com>
2024-03-11 15:44:33 +0100

    Deployed Function to Azure

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,10 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/HttpTriggerCounterBS';
+
+const functionApi = 'https://httptriggercounterbs.azurewebsites.net/api/httptriggercounterbs?code=xowlwimrW8Wu6cmtVnXRM6YWLyi0BY8vri4bN86li85fAzFuD58lcA%3D%3D';
+const localfunctionApi = 'http://localhost:7071/api/HttpTriggerCounterBS';
+
 
 const getVisitCount = () =>{
     let count = 30;
